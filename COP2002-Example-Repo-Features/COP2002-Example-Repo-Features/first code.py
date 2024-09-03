1d8be311cf57f91d2af49d151d3c2219de732fa6
JadenDonda <jaden.donda@go.sfcollege.edu>
2024-09-03 19:11:15 -0400

    added space
    added space

--- COP2002-Example-Repo-Features/COP2002-Example-Repo-Features/first code.py
+++ COP2002-Example-Repo-Features/COP2002-Example-Repo-Features/first code.py
@@ -1,7 +1,2 @@
-# Creating a project for GitHub
-# By Jaden Donda  
-      
-print("COP2002 Fall 2024")
 
-print("I'm understanding how to use GitHub to keep track of changes!") 
-... 
+print(hello_world())
